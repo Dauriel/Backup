0ae9fb8392ec7a2330056741f6082f460e79ad46
Dauriel <pabloiz@live.com>
2017-04-21 01:13:33 +0200

    Update SeatsController.java

--- src/controller/SeatsController.java
+++ src/controller/SeatsController.java
@@ -9,10 +9,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 
 /**
  * FXML Controller class
@@ -22,6 +26,8 @@ import javafx.stage.Stage;
 public class SeatsController implements Initializable {
 
     private Stage primaryStage;
+    @FXML
+    private GridPane grid;
 
     /**
      * Initializes the controller class.
@@ -32,18 +38,30 @@ public class SeatsController implements Initializable {
     }
 
     public void initStage(Stage stage) {
-        primaryStage = stage;
-        GridPane gridp = new GridPane();
-        
-        for (int i = 0; i < 19; i++) {
-            for (int j = 0; i < 13; j++) {
+        grid.setPrefSize(600, 850);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(10,30,10,10));
+        grid.setAlignment(Pos.CENTER);
+        for (int i = 1; i < 19; i++) {
+            Text x = new Text();
+            x.setText("" + i);
+            grid.add(x, 0, i);
+            grid.setHalignment(x, HPos.RIGHT);
+        }
+        for (int i = 1; i < 13; i++) {
+            Text x = new Text();
+            x.setText("" + i);
+            grid.add(x, i, 0);
+            grid.setHalignment(x, HPos.CENTER);
+            grid.setValignment(x, VPos.BOTTOM);
+        }
+        for (int i = 1; i < 13; i++) {
+            for (int j = 1; j < 19; j++) {
                 Pane x = new Pane();
-                x.setStyle("-fx-background-color: red;");
-                gridp.add(x, j, i);
+                x.setStyle("-fx-background-color: indianred;");
+                grid.add(x, i, j);
             }
         }
-        Scene aux = new Scene(gridp,800,600);
-        primaryStage.setScene(aux);
     }
-
 }
